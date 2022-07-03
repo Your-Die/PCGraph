305223f50853ffb199f72cdb25d346342e8e5d1a
jordy <jordy_van_opstal@live.nl>
2022-07-04 00:50:02 +0200

    add subGraphNode

---
+++ Runtime/Nodes/SubgraphNode.cs
@@ -0,0 +1,17 @@
+namespace Chinchillada.PCGraph
+{
+    using GraphProcessor;
+
+    public abstract class SubgraphNode<T> : GeneratorNode<T>, IUsesRNG
+    {
+        [Input, ShowAsDrawer] public PCGraph subGraph;
+
+        public IRNG RNG { get; set; }
+
+        public override T Generate()
+        {
+            var processor = new PCGraphProcessor(this.subGraph, this.RNG);
+            return processor.Generate<T>();
+        }
+    }
+}
