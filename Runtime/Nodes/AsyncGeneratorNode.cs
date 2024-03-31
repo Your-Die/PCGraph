4eff93fde5110c3c130363364a862a358e712063
Jordy <jordy_van_opstal@live.nl>
2024-04-01 00:51:12 +0200

    Fix bug in AsyncGeneratorNode
    OnGenerate wasn't being called from non-async Generate

--- Runtime/Nodes/AsyncGeneratorNode.cs
+++ Runtime/Nodes/AsyncGeneratorNode.cs
@@ -8,22 +8,32 @@ namespace Chinchillada.PCGraphs
     [Serializable]
     public abstract class AsyncGeneratorNode<T> : GeneratorNode<T>, IAsyncNode
     {
-        public override T Generate() => this.GenerateAsync().Last();
+        public override T Generate()
+        {
+            this.OnBeforeGenerate();
+
+            T result = this.GenerateAsync().Last();
+            this.OnGenerate(result);
+
+            this.InvokeOnProcessed();
+            return result;
+        }
 
         public virtual int ExpectedIterations => 1;
+
         public IEnumerator OnProcessAsync(int ticksPerFrame)
         {
             int ticksToGo = ticksPerFrame;
-            
+
             this.OnBeforeGenerate();
-            
+
             foreach (T result in this.GenerateAsync())
             {
                 this.OnGenerate(result);
 
                 if (--ticksToGo > 0)
                     continue;
-                
+
                 this.InvokeOnProcessed();
                 yield return null;
                 ticksToGo = ticksPerFrame;
@@ -34,7 +44,9 @@ namespace Chinchillada.PCGraphs
         }
 
         protected abstract IEnumerable<T> GenerateAsync();
-        
-        protected virtual void OnBeforeGenerate() {}
+
+        protected virtual void OnBeforeGenerate()
+        {
+        }
     }
 }
