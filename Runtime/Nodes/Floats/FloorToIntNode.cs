3f60408d3e32abccbaa3af2141899dba759d2d42
Jordy van Opstal <jordy_van_opstal@live.nl>
2024-05-02 10:11:58 +0200

    add floorToInt

---
+++ Runtime/Nodes/Floats/FloorToIntNode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Chinchillada.PCGraphs;
+using GraphProcessor;
+using UnityEngine;
+
+namespace Chinchillada.PCGraph.Nodes.Floats
+{
+    [Serializable, NodeMenuItem("Floats/Floor To Int")]
+    public class FloorToIntNode : GeneratorNode<int>
+    {
+        [Input] public float floatValue;
+        
+        public override int Generate() => Mathf.FloorToInt(this.floatValue);
+    }
+}
